1692c059e8fd06003a4fa46196e8d81aa4b715f5
zehraouianas <166992538+zehraouianas@users.noreply.github.com>
2024-04-21 20:59:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('Homepage.html')
-    
+
+@app.route('/calendrier')
+def calendrier():
+    return render_template('calendrier.html')
+
+@app.route('/prof_par_etu')
+def prof_par_etu():
+    return render_template('prof_par_etu.html')
+
+@app.route('/prof_filiere')
+def prof_filiere():
+    return render_template('prof_filiere.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
